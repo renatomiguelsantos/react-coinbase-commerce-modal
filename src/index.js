76e02fb73f63c523f827acbabc0b5007628b302d
Will Dembinski <willdembinski@gmail.com>
2019-01-21 21:38:53 -0800

    Fix state handling

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // @flow
-import * as React from 'react';
-import IFrame from './components/IFrame';
-import type {MessageData} from './types';
+import * as React from "react";
+import IFrame from "./components/IFrame";
+import type { MessageData } from "./types";
 
 type Props = {
   showModal?: boolean,
@@ -11,46 +11,75 @@ type Props = {
   chargeId?: string,
   customMetadata?: string,
   onLoad: () => void,
-  onChargeSuccess?: (MessageData) => void,
-  onChargeFailure?: (MessageData) => void,
-  onPaymentDetected?: (MessageData) => void,
+  onChargeSuccess?: MessageData => void,
+  onChargeFailure?: MessageData => void,
+  onPaymentDetected?: MessageData => void,
   onModalClosed?: () => void,
   disableCaching: true
 };
+type State = {
+  showModal: boolean
+};
 
-type State = {};
-
-class CoinbaseCommerceModal extends React.Component<Props, State>{
-  constructor(props: Props){
+class CoinbaseCommerceModal extends React.Component<Props, State> {
+  constructor(props: Props) {
     super(props);
+
+    this.state = {
+      showModal: !!this.props.showModal
+    };
   }
 
+  handleButtonClick = () => {
+    this.setState({ showModal: true });
+  };
+
   handleModalClose = () => {
-    if (this.props.onModalClosed){
+    this.setState({ showModal: false });
+    if (this.props.onModalClosed) {
       this.props.onModalClosed();
     }
   };
 
-  /*
-   * If we experience an unexpected error,
-   * log it as an error to the console and close the modal.
-   */
   handleError = (msg: MessageData) => {
     console.error(msg);
+    this.setState({ showModal: false });
   };
 
-  render(){
-    const {showModal} = this.props;
-    const {onLoad, onChargeSuccess, onChargeFailure, checkoutId, chargeId, customMetadata, onPaymentDetected, disableCaching} = this.props;
-    const iFrameProps = {onLoad, onChargeSuccess, onChargeFailure, checkoutId, chargeId, onPaymentDetected, disableCaching};
+  render() {
+    const { showModal } = this.state;
+    const {
+      onLoad,
+      onChargeSuccess,
+      onChargeFailure,
+      checkoutId,
+      chargeId,
+      customMetadata,
+      onPaymentDetected,
+      disableCaching
+    } = this.props;
+    const iFrameProps = {
+      onLoad,
+      onChargeSuccess,
+      onChargeFailure,
+      checkoutId,
+      chargeId,
+      onPaymentDetected,
+      disableCaching
+    };
     return (
       <div>
         {showModal && (
-          <IFrame {...iFrameProps} onModalClose={this.handleModalClose} onError={this.handleError} customMetadata={customMetadata}/>
+          <IFrame
+            {...iFrameProps}
+            onModalClose={this.handleModalClose}
+            onError={this.handleError}
+            customMetadata={customMetadata}
+          />
         )}
       </div>
-    )
+    );
   }
 }
 export default CoinbaseCommerceModal;
-export type {MessageData};
+export type { MessageData };
